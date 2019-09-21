1666b4f441928c36d66f6079d01c9f0fd18460df
Bahrami Nia Payam (HES) <payam.bahramin@hes.adhes.hesge.ch>
2019-09-21 09:01:03 +0200

    mon premier commit

--- 8.1- Exercice - Séquence 1/sequence1.py
+++ 8.1- Exercice - Séquence 1/sequence1.py
@@ -1,6 +1,7 @@
 # Problème : Etant donné une variable a et b, on demande de mettre la valeur de a dans b et celle de b dans a
 # Contraintes : Ne pas utiliser de valeurs numériques.
 # Données : les variables a et b
+# Auteur: payam bahrami
 
 a = 11
 b = 42
